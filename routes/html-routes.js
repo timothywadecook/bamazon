9eabab65ccc863982553f465d0a329d16b3bb29d
Timothy Cook <tim.wade.cook@gmail.com>
2019-02-26 17:28:20 -0500

    added the appjs file to routes to serve it up to html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -17,6 +17,10 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 
+  app.get('/appjs', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/js/app.js'))
+  });
+
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   });

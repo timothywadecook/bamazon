319cc4918f10853652466e61489b00e9900a2a49
Timothy Cook <tim.wade.cook@gmail.com>
2019-02-26 21:19:06 -0500

    product id == not === in add to cart

--- public/js/app.js
+++ public/js/app.js
@@ -114,10 +114,7 @@ const runAddToCart = function(e) {
     e.preventDefault();
     const productId = e.target.id;
     const qty = $(`#i${productId}`).val(); // store input field
-    const productData = cachedProductList.find(prod => {prod.id === productId}); // next wrap this here
-    console.log('just check cachedProductList exist..',cachedProductList);
-    console.log('qty = ',qty);
-    console.log('product id = ',productId);
+    const productData = cachedProductList.find(prod => prod.id == productId); // next wrap this here
     console.log('productData should be object =', productData);
     console.log('stock quanity should be number = ',productData.stock_quantity)
     $(`#i${productId}`).val(''); // empty input field

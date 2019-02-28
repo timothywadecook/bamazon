1da1fd1d517bdf1507ca0f15127ef09264f93e60
Timothy Cook <tim.wade.cook@gmail.com>
2019-02-28 10:10:17 -0500

    force true removed server db sync to avoid dropping db on restart

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ require('./routes/html-routes')(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true })
+db.sequelize.sync({})
 .then(function() {
     app.listen(PORT, function() {
       console.log('App listening on PORT ' + PORT);
